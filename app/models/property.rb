c3b5d34ebd2ee627f177f2311eeb9e167c479dfb
takumi aramaki <armktk0329m@gmail.com>
2019-07-12 13:33:16 +0900

    エラーメッセージとNOTNULL制約、数値入力カラムに文字列が登録できないようにバリデーションを追加

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,4 +1,16 @@
 class Property < ApplicationRecord
   has_many :stations
   accepts_nested_attributes_for :stations
+  with_options presence: true do
+    validates :name
+    validates :rent
+    validates :age
+    validates :address
+    validates :remark
+  end
+
+  with_options numericality: true do
+    validates :rent
+    validates :age
+  end
 end
